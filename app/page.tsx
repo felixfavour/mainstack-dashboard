557fa266cf3c4271eddfd3d48e9ccaf04dc7644b
felixfavour <felixfavour0@gmail.com>
2023-11-26 22:07:51 +0100

    chore: added icons + restructure app dir

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import MainstackLogo from './components/Icons/Logo'
+import styles from './styles/page.module.css'
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <MainstackLogo />
       </div>
     </main>
   )
