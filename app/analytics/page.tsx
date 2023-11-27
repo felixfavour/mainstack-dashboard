2b94aa018ad680227960a628c1a01b2eaf7e1458
felixfavour <felixfavour0@gmail.com>
2023-11-27 06:25:56 +0100

    chore: added empty pages to avoid 404

---
+++ app/analytics/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import styles from '../styles/page.module.css'
+
+export default function Page() {
+
+  return (
+    <section className={styles.section}>
+      <div className={styles.inner}>
+      </div>
+    </section>
+  )
+}
