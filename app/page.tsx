98bf49ec39110e2d212dbcd5afb3529987a1bf79
felixfavour <felixfavour0@gmail.com>
2023-11-29 09:32:56 +0100

    chore: auto redirect and updated app favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import styles from './styles/page.module.css'
 
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/revenue')
+  }, [])
   return (
     <section className={styles.main}>
       <div className={styles.description}>
