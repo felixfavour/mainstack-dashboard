5b665841fa098369f29970b8b1b67d0c9725780e
felixfavour <felixfavour0@gmail.com>
2023-11-29 10:07:27 +0100

    fix: safari button focus issue -x2

--- app/components/CustomSelect.tsx
+++ app/components/CustomSelect.tsx
@@ -17,10 +17,10 @@ interface Props {
 export default function CustomSelect({ label, value, options, onChange }: Props) {
   return (
     <div className={styles.custom_select__ctn} onSubmit={(e) => e.preventDefault()}>
-      <div className={styles.select__bar} onClick={(event) => event.currentTarget.focus()}>
+      <button className={styles.select__bar} onClick={(event) => event.currentTarget.focus()}>
         <span>{value?.toString()?.replaceAll(',', ', ') || label}</span>
         <ChevronDownIcon />
-      </div>
+      </button>
       <div className={styles.options}>
         {options?.map(option => <button
           key={option}
